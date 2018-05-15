d9521015d54bd0e86900e0f8ea3eb2c2b3fe920d
satoshi.iwasaki <satoshi.iwasaki@glpgs.com>
2018-05-15 13:31:53 +0900

    add slot of error-rate in qubit

--- simulator/src/class.lisp
+++ simulator/src/class.lisp
@@ -3,7 +3,8 @@
 (defclass qubit ()
   ((x :accessor x :initarg :x :initform 0)
    (y :accessor y :initarg :y :initform 0)
-   (z :accessor z :initarg :z :initform -1)))
+   (z :accessor z :initarg :z :initform -1)
+   (error-rate :accessor error-rate :initarg :error-rate :initform 0.0)))
 
 (defclass qugate () ())
 (defclass gate-h (qugate) ())
