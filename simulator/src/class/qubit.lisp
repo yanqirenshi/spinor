c6dd470289f9092f9fc89ae3e284a15ebf9ec065
satoshi.iwasaki <satoshi.iwasaki@glpgs.com>
2018-07-25 15:16:57 +0900

    add qubit so3 and su2

--- simulator/src/class/qubit.lisp
+++ simulator/src/class/qubit.lisp
@@ -6,4 +6,11 @@
 (defclass qubit (bloch-sphere)
   ((error-rate :accessor error-rate :initarg :error-rate :initform 0.0)))
 
-(defclass photon () ())
+(defclass qubit-so3 (qubit)
+  ((x :accessor x :initarg :x :initform 0.0)
+   (y :accessor y :initarg :y :initform 0.0)
+   (z :accessor z :initarg :z :initform 1.0)))
+
+(defclass qubit-su2 (qubit)
+  ((alpha :accessor alpha :initarg :alpha :initform #C(0.0 1.0))
+   (beta  :accessor beta  :initarg :beta  :initform 0.0)))
